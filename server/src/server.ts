f077e7af8d162b5efd8dd2f09b659ba7e7cd866c
Yan <yankaiquecosta.yk@gmail.com>
2020-06-03 11:14:49 -0300

    Main da aplicaçao

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,10 @@
 import express from 'express';
+import routes from './routes';
+
 const app = express();
 
-app.get('/users',(request,response)=>{
-    response.send('Hello World');
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
 

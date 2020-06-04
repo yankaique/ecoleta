518ae3fcea6ddb9656cef50dd85bc73b2525479a
Yan <yankaiquecosta.yk@gmail.com>
2020-06-04 11:30:28 -0300

    Criação de tabelas atualizadas

--- server/src/database/migrations/02_create_point_item.ts
+++ server/src/database/migrations/02_create_point_item.ts
@@ -1,7 +1,15 @@
-export async function up(){
+import Knex from 'knex';
 
+export async function up(knex:Knex){
+    // CRIAR A TABELA
+    return knex.schema.createTable('point_items',table=>{
+        table.increments('id').primary();
+        table.integer('point_id').notNullable().references('id').inTable('points');
+        table.integer('item_id').notNullable().references('id').inTable('points');
+    });
 }
 
-export async function down(){
-    
+export async function down(knex:Knex){
+    // VOLTAR ATRAS (DELETAR TABELA)
+    return knex.schema.dropTable('point_items');
 }

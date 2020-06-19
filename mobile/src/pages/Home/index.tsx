6292d169d2457ac34a98aa4165070fc0a5922cf9
Yan <yankaiquecosta.yk@gmail.com>
2020-06-19 11:45:45 -0300

    Adicionado função para levar até a página points

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -2,9 +2,17 @@ import React from 'react';
 import {Feather as Icon } from '@expo/vector-icons'
 import {View, Text,Image, StyleSheet, ImageBackground} from 'react-native';
 import { RectButton} from 'react-native-gesture-handler';
+import { useNavigation} from '@react-navigation/native';
 
 
 const Home=()=>{
+
+    const navigation = useNavigation();
+
+    function handleNavigateToPoits(){
+      navigation.navigate('Points');
+    }
+
     return(
         <ImageBackground style={styles.container} 
             source={require('../../assets/home-background.png')} 
@@ -18,7 +26,7 @@ const Home=()=>{
             </View>
 
             <View style={styles.footer}>
-                <RectButton style={styles.button} onPress={()=>{}}>
+                <RectButton style={styles.button} onPress={handleNavigateToPoits}>
                     <View style={styles.buttonIcon}>
                         <Icon name="arrow-right" color="#FFF" size={24}/>
                     </View> 
@@ -36,7 +44,6 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       padding: 32,
-      backgroundColor:"#f0f0f5"
     },
   
     main: {
